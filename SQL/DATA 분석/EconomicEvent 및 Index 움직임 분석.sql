8865c5ba325160450f54ddd9583385ab0981c321
pardon6 <trader.sjryu@gmail.com>
2019-09-03 17:06:14 +0900

    Create EconomicEvent 및 Index 움직임 분석.sql

---
+++ SQL/DATA 분석/EconomicEvent 및 Index 움직임 분석.sql
@@ -0,0 +1,32 @@
+
+SELECT a.event_nm, a.event_cd, a.index_cd
+     , (a.pos_cnt+b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS pos_hit
+	  , c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_hit
+	  , d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg2_hit
+	  , (a.pos_ratio*a.pos_cnt - b.neg_ratio*b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS pos_ratio
+	  , c.w_ratio*c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_ratio
+	  , d.w_ratio*d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_ratio
+  FROM
+  (SELECT event_nm, event_cd, index_cd, COUNT(*) AS pos_cnt, AVG(index_value_ratio) AS pos_ratio
+     FROM economic_events_results
+    WHERE event_value_diff > 0 AND index_value_ratio > 0
+    GROUP BY event_nm, event_cd, index_cd) a
+, (SELECT event_cd, index_cd, COUNT(*) AS neg_cnt, AVG(index_value_ratio) AS neg_ratio
+     FROM economic_events_results
+    WHERE event_value_diff < 0 AND index_value_ratio < 0
+    GROUP BY event_cd, index_cd) b
+, (SELECT event_cd, index_cd, COUNT(*) AS w_cnt, AVG(index_value_ratio) AS w_ratio
+     FROM economic_events_results
+    WHERE event_value_diff > 0 AND index_value_ratio < 0
+    GROUP BY event_cd, index_cd) c
+, (SELECT event_cd, index_cd, COUNT(*) AS w_cnt, AVG(index_value_ratio) AS w_ratio
+     FROM economic_events_results
+    WHERE event_value_diff < 0 AND index_value_ratio > 0
+    GROUP BY event_cd, index_cd) d
+ WHERE a.event_cd = b.event_cd
+   AND a.index_cd = b.index_cd
+   AND a.event_cd = c.event_cd
+   AND a.index_cd = c.index_cd  
+   AND a.event_cd = d.event_cd
+   AND a.index_cd = d.index_cd  
+  
