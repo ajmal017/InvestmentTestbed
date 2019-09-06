1d0931ce6e3884e9cf36a60edb8ae61cae7abe79
pardon6 <trader.sjryu@gmail.com>
2019-09-06 12:42:46 +0900

    Update EconomicEvent 및 Index 움직임 분석.sql

--- SQL/DATA 분석/EconomicEvent 및 Index 움직임 분석.sql
+++ SQL/DATA 분석/EconomicEvent 및 Index 움직임 분석.sql
@@ -1,11 +1,11 @@
 
-SELECT a.event_nm, a.event_cd, a.index_cd
-     , (a.pos_cnt+b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS pos_hit
-	  , c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_hit
-	  , d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg2_hit
-	  , (a.pos_ratio*a.pos_cnt - b.neg_ratio*b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS pos_ratio
-	  , c.w_ratio*c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_ratio
-	  , d.w_ratio*d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt) AS neg1_ratio
+SELECT a.event_nm, a.event_cd, e.nm_us AS index_nm, a.index_cd
+     , FORMAT((a.pos_cnt+b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS pos_hit
+	  , FORMAT(c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS neg1_hit
+	  , FORMAT(d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS neg2_hit
+	  , FORMAT((a.pos_ratio*a.pos_cnt - b.neg_ratio*b.neg_cnt)/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS pos_ratio
+	  , FORMAT(c.w_ratio*c.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS neg1_ratio
+	  , FORMAT(d.w_ratio*d.w_cnt/(a.pos_cnt+b.neg_cnt+c.w_cnt+d.w_cnt),4) AS neg1_ratio
   FROM
   (SELECT event_nm, event_cd, index_cd, COUNT(*) AS pos_cnt, AVG(index_value_ratio) AS pos_ratio
      FROM economic_events_results
@@ -23,10 +23,12 @@ SELECT a.event_nm, a.event_cd, a.index_cd
      FROM economic_events_results
     WHERE event_value_diff < 0 AND index_value_ratio > 0
     GROUP BY event_cd, index_cd) d
+, index_master e
  WHERE a.event_cd = b.event_cd
    AND a.index_cd = b.index_cd
    AND a.event_cd = c.event_cd
    AND a.index_cd = c.index_cd  
    AND a.event_cd = d.event_cd
    AND a.index_cd = d.index_cd  
+   AND a.index_cd = e.cd
   
