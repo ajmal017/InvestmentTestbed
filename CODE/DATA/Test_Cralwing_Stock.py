49b2cab221ae6f934223eeb3df06e73f353e2143
pardon6 <trader.sjryu@gmail.com>
2020-02-19 17:17:25 +0900

    Update Test_Cralwing_Stock.py

--- CODE/DATA/Test_Cralwing_Stock.py
+++ CODE/DATA/Test_Cralwing_Stock.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore")
 from COMM import DB_Util
 from CRAWLING import Investing
 
-
+loop_sleep_term = 1
 do_profile = True
 do_financial = False
 do_earnings = False
@@ -35,44 +35,43 @@ for idx, option in enumerate(options):
 
     country = option[0]
     group = option[1]
-
+    
+    # 종목리스트 생성을 위한 국가별 대표지수 설정
     obj.SetCountryGroupInfo(country, group)
 
-
-
-    # 주식의 기본 정보를 새로 크롤링 함.
+    # 주식의 기본 정보 크롤링
     if do_profile == True:
 
+        # 대표지수에 포함되어 있는 종목 리스트 및 필요 정보 크롤링
         columns = ['pid', 'country', 'nm', 'industry', 'sector', 'url', 'profile_url', 'financial_url', 'earnings_url']
         comp_info_list = obj.GetCompsInfo(columns, idx)
 
         for idx_comp, comp_info in comp_info_list.iterrows():
-
             '''
             # 요청이 너무 많은 경우 원격 호스트에 의해 강제로 끊는담.
             # 처리된 종목까지는 패스
-            if option[1] == 'S&P 500' and idx_comp < 312:
+            if option[1] == 'S&P 500' and idx_comp < 441:
                 continue
             '''
-
-            # 동일 종목이 여러 지수에 중복 편입되어 있는 경우 패스
+            # 동일 종목이 기존에 처리된 지수에 중복 편입되어 있는 경우 패스
             if comp_info['pid'] in stocks_list:
                 print(comp_info['nm'] + '는 이미 등록되어 있음.')
                 continue
             else:
                 stocks_list.append(comp_info['pid'])
-
+            
+            # 종목의 산업과 업종 정보 크롤링
             comp_info = obj.GetProfileData(comp_info['profile_url'], comp_info)
             #print(profile)
 
-
+            # 크롤링된 종목 정보를 DB 저장
             sql = "INSERT INTO stock_master (pid, country, nm, industry, sector, url, profile_url, financial_url, earnings_url)" \
                   "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" \
                   "ON DUPLICATE KEY UPDATE country='%s', nm='%s', industry='%s', sector='%s', url='%s', profile_url='%s', financial_url='%s', earnings_url='%s'"
             sql_arg = (comp_info['pid'], option[0], comp_info['nm'], comp_info['industry'], comp_info['sector'], comp_info['url'], comp_info['profile_url'], comp_info['financial_url'], comp_info['earnings_url']
                        , option[0], comp_info['nm'], comp_info['industry'], comp_info['sector'], comp_info['url'], comp_info['profile_url'], comp_info['financial_url'], comp_info['earnings_url'])
             if (db.execute_query(sql, sql_arg) == False):
-                print("stock_master insert error")
+                print("stock_master insert error(%s: %s)" % (comp_info['pid'], comp_info['nm']))
             else:
                 print('idx:\t' + str(idx_comp) + '\tnm\t' + comp_info['nm']
                       + '\tindustry\t' + comp_info['industry'] + '\tsector\t' + comp_info['sector']
@@ -80,6 +79,10 @@ for idx, option in enumerate(options):
                       + '\tprofile_url:\t' + comp_info['profile_url']
                       + '\tfinancial_url:\t' + comp_info['financial_url']
                       + '\tearnings_url:\t' + comp_info['earnings_url'])
+
+            time.sleep(loop_sleep_term)
+
+    # 사전에 크롤링된 종목 정보를 DB에서 가져옴
     else:
         sql = "SELECT pid, country, nm, industry, sector, url, profile_url, financial_url, earnings_url" \
               "  FROM stock_master"
@@ -88,15 +91,18 @@ for idx, option in enumerate(options):
 
 
     if do_financial == True:
-        financial = obj.GetFinancialData(comp_info['financial_url'])
-        print(financial)
+        for idx_comp, comp_info in comp_info_list.iterrows():
+            financial = obj.GetFinancialData(comp_info['financial_url'])
+            print(financial)
+
+            time.sleep(loop_sleep_term)
 
     if do_earnings == True:
-        earnings = obj.GetEarningsData(comp_info['earnings_url'], t_gap=0.1, loop_num=1)
-        print(earnings)
+        for idx_comp, comp_info in comp_info_list.iterrows():
+            earnings = obj.GetEarningsData(comp_info['earnings_url'], t_gap=0.1, loop_num=1)
+            print(earnings)
 
-        time.sleep(5)
-
-        #break
+            time.sleep(loop_sleep_term)
+    #break
 
 db.disconnect()
